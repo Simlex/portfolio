fda85751ced623de22cfe1013c226fcb2f882024
Simlex <simlexafol@gmail.com>
2025-01-03 17:08:03 +0100

    Replace local fonts with Google Fonts for improved performance and consistency

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,8 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
+import { Inter } from "next/font/google";
 import "./globals.css";
-import Layout from "./components/shared/Layout";
 
-const geistSans = localFont({
-    src: "./fonts/GeistVF.woff",
-    variable: "--font-geist-sans",
-    weight: "100 900",
-});
-const geistMono = localFont({
-    src: "./fonts/GeistMonoVF.woff",
-    variable: "--font-geist-mono",
-    weight: "100 900",
-});
+const leagueSpartan = Inter({ display: "swap", subsets: ["latin"] });
 
 export const metadata: Metadata = {
     title: "Simlex Portfolio Website",
@@ -27,7 +17,7 @@ export default function RootLayout({
     return (
         <html lang="en">
             <body
-                className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+                className={`${leagueSpartan.className} antialiased`}
             >
                 {/* <Layout children={children} /> */}
                 {children}
