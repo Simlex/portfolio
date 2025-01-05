d78bdf4d3174fab6c6011a5952731153249f5ed5
Simlex <simlexafol@gmail.com>
2025-01-06 00:25:30 +0100

    Add new image imports to enhance visual content

--- public/images.ts
+++ public/images.ts
@@ -1,5 +1,15 @@
 import e_ticketsdeck from "./images/e_ticketsdeck.png";
+import bablo from "./images/bablo.png";
+import t247 from "./images/247.png";
+import mcnif from "./images/mcnif.png";
+import portfolio from "./images/portfolio.png";
+import stylist_p from "./images/stylist_p.png";
 
 export default {
   e_ticketsdeck,
+  bablo,
+  t247,
+  mcnif,
+  portfolio,
+  stylist_p
 };
